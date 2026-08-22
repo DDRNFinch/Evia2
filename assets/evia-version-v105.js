f91cd830832cb93bd78a0549c92cf79c166ae553
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-22 21:41:23 +0100

    Bump Evia app version to 108

--- assets/evia-version-v105.js
+++ assets/evia-version-v105.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","107");window.EviaAppVersion=107})();
+(()=>{"use strict";const meta=document.querySelector('meta[name="evia-app-version"]');if(meta)meta.setAttribute("content","108");window.EviaAppVersion=108})();
